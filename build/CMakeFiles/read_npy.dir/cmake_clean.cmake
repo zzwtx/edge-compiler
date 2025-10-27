afdb9b2fea7c93b0bd66f2e1e8e91fa5641ed1d1
zzwtx <3555729149@qq.com>
2025-10-27 11:24:20 +0800

    TensorRT_runner

---
+++ build/CMakeFiles/read_npy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_npy.dir/cpp_read_npy_cnpy.cpp.o"
+  "CMakeFiles/read_npy.dir/cpp_read_npy_cnpy.cpp.o.d"
+  "read_npy"
+  "read_npy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/read_npy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/tensorrt_runner.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tensorrt_runner.dir/TensorRT.cpp.o"
+  "CMakeFiles/tensorrt_runner.dir/TensorRT.cpp.o.d"
+  "tensorrt_runner"
+  "tensorrt_runner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tensorrt_runner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
